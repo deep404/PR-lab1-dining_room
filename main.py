92a6a450d1eea0618c66211af91b985a9e439eeb
yourbeach <45741388+yourbeach@users.noreply.github.com>
2021-10-20 00:06:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,111 @@
-import config as config
-from actors.DinningHall import DinningHall
-from actors.Waiter import Waiter
-import logging
-from flask import Flask, request
+import requests
 import threading
+from flask import Flask, request
+import logging
+import coloredlogs
+import addons
+from actors.DinningHall import DinningHall
 
-logging.basicConfig(filename='dinning.log', level=logging.DEBUG, format='%(asctime)s:  %(message)s', datefmt="%m/%d/%Y %I:%M:%S %p")
+logging.basicConfig(filename='dinning.log', level=logging.DEBUG, format='%(asctime)s: %(message)s', datefmt="%m/%d/%Y %I:%M:%S %p")
 logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG')
+
+restaurants = addons.get_restaurant()
+
+HOST = '0.0.0.0'
+FO_PORT = 3000
+# FO_HOST = 'localhost'
+FO_HOST = 'food_ordering'
+
+app1 = Flask(restaurants[0]['name'])
+app2 = Flask(restaurants[1]['name'])
+app3 = Flask(restaurants[2]['name'])
+app4 = Flask(restaurants[3]['name'])
+
+d1 = DinningHall(restaurants[0])
+d2 = DinningHall(restaurants[1])
+d3 = DinningHall(restaurants[2])
+d4 = DinningHall(restaurants[3])
+
+dinning_halls = [{
+    'app': app1,
+    'dh': d1
+}, {
+    'app': app2,
+    'dh': d2
+}, {
+    'app': app3,
+    'dh': d3
+}, {
+    'app': app4,
+    'dh': d4
+}]
+
+# set up each Dinning-Hall (dh) endpoints
+for i, app_data in enumerate(dinning_halls):
+    app = app_data['app']
+    dh = app_data['dh']
+
+    # POST /distribution : exposed for Kitchens
+    @app.route('/distribution', methods=['POST'])
+    def distribution(app_dh=dh):
+        order = request.get_json()
+        return app_dh.distribution(order)
+
+    # GET /menu            : exposed for Client Service
+    @app.route('/menu', methods=['GET'])
+    def menu(app_dh=dh):
+        return app_dh.get_menu()
+
+    # POST /v2/order       : exposed for Client Service
+    @app.route('/v2/order', methods=['POST'])
+    def order_v2(app_dh=dh):
+        data = request.get_json()
+        return app_dh.order(data)
+
+    # GET /v2/order/id     : exposed for Food Ordering
+    @app.route('/v2/order/<order_id>', methods=['GET'])
+    def get_order(order_id, app_dh=dh):
+        return app_dh.get_order(order_id)
+
+    # GET /restaurant_data : exposed for Kitchens
+    @app.route('/restaurant_data', methods=['GET'])
+    def get_restaurant_data(app_dh=dh):
+        # start generating orders once the kitchen was configured
+        threading.Thread(target=app_dh.generate_dh_order, daemon=True).start()
+        return app_dh.get_restaurant_data()
+
+    # POST /rating         : exposed for Client Service
+    @app.route('/rating', methods=['POST'])
+    def rating(app_dh=dh):
+        data = request.get_json()
+        return app_dh.update_rating(data)
 
 
-app = Flask(__name__)
-
-threads = []
-
-
-@app.route('/distribution', methods=['POST'])
-def distribution():
-    order = request.get_json()
-    logger.info(f'NEW DISTRIBUTION orderId: {order["order_id"][0:4]}')
-
-    # update table state
-    table_idx = next((i for i, table in enumerate(config.TABLES_LIST) if table['id'] == order['table_id']), None)
-    config.TABLES_LIST[table_idx]['state'] = config.TABLE_WAITING_FOR_ORDER_TO_BE_SERVED
-
-    # get the waiter thread and serve order
-    waiter_thread: Waiter = next((w for w in threads if type(w) == Waiter and w.id == order['waiter_id']), None)
-    waiter_thread.serve_order(order)
-    return {'isSuccess': True}
+def register_all():
+    rests = addons.get_restaurant()
+    requests.post(f'http://{FO_HOST}:{FO_PORT}/register', json=rests[0])
+    requests.post(f'http://{FO_HOST}:{FO_PORT}/register', json=rests[1])
+    requests.post(f'http://{FO_HOST}:{FO_PORT}/register', json=rests[2])
+    requests.post(f'http://{FO_HOST}:{FO_PORT}/register', json=rests[3])
+    logger.info(f'Registered {len(rests)} restaurants\n')
 
 
-def start_dinning():
-    flask_thread = threading.Thread(target=lambda: app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False), daemon=True)
-    threads.append(flask_thread)
+def main():
+    open("dinning.log", "w").close()
 
-    dh_thread = DinningHall()
-    threads.append(dh_thread)
+    # each dinning hall is a separate flask application
+    threading.Thread(target=lambda: app1.run(host=HOST, port=d1.config['dinning_port'], debug=False, use_reloader=False, threaded=True), name=f'FLASK-DH1', daemon=True).start()
+    threading.Thread(target=lambda: app2.run(host=HOST, port=d2.config['dinning_port'], debug=False, use_reloader=False, threaded=True), name=f'FLASK-DH2', daemon=True).start()
+    threading.Thread(target=lambda: app3.run(host=HOST, port=d3.config['dinning_port'], debug=False, use_reloader=False, threaded=True), name=f'FLASK-DH3', daemon=True).start()
+    threading.Thread(target=lambda: app4.run(host=HOST, port=d4.config['dinning_port'], debug=False, use_reloader=False, threaded=True), name=f'FLASK-DH4', daemon=True).start()
 
-    for _, w in enumerate(config.WAITER_LIST):
-        waiter_thread = Waiter(w)
-        threads.append(waiter_thread)
+    # register 'em to food ordering service
+    register_all()
 
-    for th in threads:
-        th.start()
-    for th in threads:
-        th.join()
+    while True:
+        pass
 
 
 if __name__ == '__main__':
-    start_dinning()
+    main()
