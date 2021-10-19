22255690fc8c8a9d608c20c70658748b53818009
yourbeach <45741388+yourbeach@users.noreply.github.com>
2021-10-20 00:08:05 +0300

    Update Waiter.py

--- actors/Waiter.py
+++ actors/Waiter.py
@@ -1,81 +1,79 @@
-import time
-import random
-import threading
 import requests
-import queue
 import logging
-import config as config
+import random
+import time
 
 
 logger = logging.getLogger(__name__)
 
+KITCHEN_HOST = 'kitchen'
 
-class Waiter(threading.Thread):
-    def __init__(self, info, *args, **kwargs):
-        super(Waiter, self).__init__(*args, **kwargs)
-        self.name = info['name']
-        self.id = info['id']
-        self.daemon = True
 
-    def run(self):
-        while True:
-            self.search_order()
+class Waiter:
+    def __init__(self, dh, i):
+        self.name = f'{dh.name} Waiter-{i}'
+        self.dh = dh
+        self.id = i
+        self.TIME_UNIT = 1
 
     def serve_order(self, order_to_serve):
         # check if the order is the same order that what was requested
-        req_order = next((order for i, order in enumerate(config.ORDER_LIST) if order['id'] == order_to_serve['order_id']), None)
+        req_order = next((order for i, order in enumerate(self.dh.orders) if order['id'] == order_to_serve['order_id']), None)
         if req_order is not None and req_order['items'].sort() == order_to_serve['items'].sort():
             # update table state
-            table_idx = next((i for i, table in enumerate(config.TABLES_LIST) if table['id'] == order_to_serve['table_id']), None)
-            config.TABLES_LIST[table_idx]['state'] = config.TABLE_ORDER_SERVED
+            table_idx = next((i for i, table in enumerate(self.dh.tables) if table.id == order_to_serve['table_id']), None)
+            self.dh.tables[table_idx].status = 'ORDER_SERVED'
 
             # calculate total order time
             order_total_preparing_time = int(time.time() - order_to_serve['time_start'])
 
             # calculate nr of start
-            order_stars = {'order_id': order_to_serve['order_id']}
-            if order_to_serve['max_wait'] > order_total_preparing_time:
-                order_stars['star'] = 5
-            elif order_to_serve['max_wait'] * 1.1 > order_total_preparing_time:
-                order_stars['star'] = 4
-            elif order_to_serve['max_wait'] * 1.2 > order_total_preparing_time:
-                order_stars['star'] = 3
-            elif order_to_serve['max_wait'] * 1.3 > order_total_preparing_time:
-                order_stars['star'] = 2
-            elif order_to_serve['max_wait'] * 1.4 > order_total_preparing_time:
-                order_stars['star'] = 1
-            else:
-                order_stars['star'] = 0
+            stars = self.rating_stars(order_to_serve['max_wait'], order_total_preparing_time)
+            self.dh.update_rating({'order_id': order_to_serve['order_id'], 'stars': stars})
 
-            config.ORDER_STARS.append(order_stars)
-            avg = float(sum(s['star'] for s in config.ORDER_STARS)) / len(config.ORDER_STARS)
-
-            served_order = {**order_to_serve, 'total_preparing_time': order_total_preparing_time, 'status': config.ORDER_SERVED}
-            config.SERVED_ORDERS.append(served_order)
-            logger.info(f'#{self.id}-{threading.current_thread().name}-$ SERVED orderId: {served_order["order_id"][0:4]} | tableId: {served_order["table_id"]} | maxWait: {served_order["max_wait"]} | cookingTime: {served_order["cooking_time"]} | totalTime: {served_order["total_preparing_time"]} sec. [avg-star]: {avg}')
+            served_order = {**order_to_serve, 'total_preparing_time': order_total_preparing_time}
+            self.dh.done_orders.append(served_order)
+            logger.info(f'{self.name}-$ SERVED orderId: {served_order["order_id"]} | '
+                        f'table_id: {served_order["table_id"]} | '
+                        f'max_wait: {served_order["max_wait"]} | '
+                        f'total_preparing_time: {served_order["total_preparing_time"]} sec. | '
+                        f'stars | : {stars}')
         else:
-            raise Exception(f'The order is not the same as was requested. Original: {req_order}, given: {order_to_serve}')
+            raise Exception(f'{self.name} The order is not the same as was requested. Original: {req_order}, given: {order_to_serve}')
 
     def search_order(self):
-        try:
-            order = config.ORDER_Q.get()
-            config.ORDER_Q.task_done()
+        while True:
+            try:
+                order = self.dh.orders_q.get_nowait()
+                if order:
+                    time.sleep(random.randint(2, 4) * self.TIME_UNIT)
+                    table_idx = next((i for i, table in enumerate(self.dh.tables) if table.id == order['table_id']), None)
+                    logger.info(f'{self.name}-$ PICKED UP orderId: {order["id"]} | priority: {order["priority"]} | items: {order["items"]}')
+                    self.dh.tables[table_idx].status = 'ORDER_PICKED_UP'
+                    req = {
+                        'order_id': order['id'],
+                        'table_id': order['table_id'],
+                        'waiter_id': self.id,
+                        'items': order['items'],
+                        'priority': order['priority'],
+                        'max_wait': order['max_wait'],
+                        'time_start': time.time()
+                    }
+                    requests.post(f'http://{KITCHEN_HOST}:{self.dh.config["kitchen_port"]}/order', json=req, timeout=0.0000000001)
+            except Exception as e:
+                pass
 
-            table_idx = next((i for i, table in enumerate(config.TABLES_LIST) if table['id'] == order['table_id']), None)
-            logger.info(f'#{self.id}-{threading.current_thread().name}-$ PICKED UP orderId: {order["id"][0:4]} | priority: {order["priority"]} | items: {order["items"]}')
-            config.TABLES_LIST[table_idx]['state'] = config.TABLE_WAITING_FOR_ORDER_TO_BE_SERVED
-            payload = dict({
-                'order_id': order['id'],
-                'table_id': order['table_id'],
-                'waiter_id': self.id,
-                'items': order['items'],
-                'priority': order['priority'],
-                'max_wait': order['max_wait'],
-                'time_start': time.time()
-            })
-            time.sleep(random.randint(2, 4) * config.TIME_UNIT)
-            # requests.post('http://kitchen:8000/order', json=payload, timeout=0.0000000001)
-            requests.post('http://localhost:8000/order', json=payload, timeout=0.0000000001)
-
-        except (queue.Empty, requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
-            pass
+    @staticmethod
+    def rating_stars(max_wait, total):
+        stars = 0
+        if max_wait >= total:
+            stars = 5
+        elif max_wait * 1.1 >= total:
+            stars = 4
+        elif max_wait * 1.2 >= total:
+            stars = 3
+        elif max_wait * 1.3 >= total:
+            stars = 2
+        elif max_wait * 1.4 >= total:
+            stars = 1
+        return stars
